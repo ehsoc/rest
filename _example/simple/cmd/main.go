4a576811c3191ac63cd14f6d1219bf8ee5487ff2
Cristiano's <osheec@gmail.com>
2020-10-08 17:18:44 -0400

    readme: code example fix

--- _example/simple/cmd/main.go
+++ _example/simple/cmd/main.go
@@ -4,10 +4,9 @@ import (
 	"net/http"
 
 	"github.com/ehsoc/resource/_example/simple"
-	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	server := (simple.GenerateServer()
+	server := simple.GenerateServer()
 	http.ListenAndServe(":8080", server)
 }
