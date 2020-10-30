3dfe78e464b06183839f8af885d85e907a7a3bfa
Cristiano's <osheec@gmail.com>
2020-10-30 17:43:05 -0400

    fix comment

--- document_generator.go
+++ document_generator.go
@@ -5,7 +5,7 @@ import (
 )
 
 // APISpecGenerator is the interface implemented by types that transform a RestAPI type into an REST API specification,
-// in a specific format.
+// in a specific format and writing it to w.
 type APISpecGenerator interface {
 	GenerateAPISpec(w io.Writer, restAPI RestAPI)
 }
