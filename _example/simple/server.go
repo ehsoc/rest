95fafe28e63f01489e8a32298dbbba885949a0ed
Cristiano's <osheec@gmail.com>
2020-11-10 14:01:28 -0500

    fix example

--- _example/simple/server.go
+++ _example/simple/server.go
@@ -21,25 +21,31 @@ type Car struct {
 func GenerateServer() http.Handler {
 	getCarOperation := func(i rest.Input) (body interface{}, success bool, err error) {
 		carID, err := i.GetURIParam("carID")
+
 		if err != nil {
 			log.Println("error getting parameter: ", err)
 			return Car{}, false, err
 		}
+
 		if carID == "error" {
 			// Internal error trying to get the car. This will trigger a response code 500
 			return nil, false, errors.New("Internal error")
 		}
+
 		if carID != "101" {
 			// Car not found, success is false, no error. This will trigger a response code 404
 			return nil, false, nil
 		}
+
 		// Car found, success true, error nil. This will trigger a response code 200
 		return Car{carID, "Foo"}, true, nil
 	}
+	// Responses
 	successResponse := rest.NewResponse(200).WithOperationResultBody(Car{})
 	failResponse := rest.NewResponse(404)
+	// Method Operation
 	getCar := rest.NewMethodOperation(rest.OperationFunc(getCarOperation), successResponse).WithFailResponse(failResponse)
-
+	// Renderers
 	rds := rest.NewRenderers()
 	rds.Add("application/json", encdec.JSONEncoderDecoder{}, true)
 
@@ -53,7 +59,11 @@ func GenerateServer() http.Handler {
 			r.Get(getCar, rds).WithParameter(carIDParam)
 		})
 	})
-	server := api.GenerateServer(chigenerator.ChiGenerator{})
+	// Generating OpenAPI v2 specification to standard output
 	api.GenerateSpec(os.Stdout, &oaiv2.OpenAPIV2SpecGenerator{})
+
+	// Generating server routes
+	server := api.GenerateServer(chigenerator.ChiGenerator{})
+
 	return server
 }
