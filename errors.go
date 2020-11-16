8c299dd5f7ae19af8196f634e3e3ffe87270bfbe
Cristiano's <osheec@gmail.com>
2020-11-16 16:41:01 -0500

    error message fix

--- errors.go
+++ errors.go
@@ -10,12 +10,12 @@ var ErrorNoDefaultContentTypeIsSet = errors.New("no default renderer is set")
 
 // ErrorRequestBodyNotDefined error describes a specification/parameter check error trying to get the request body,
 // but it was not declared as parameter.
-var ErrorRequestBodyNotDefined = errors.New("resource: a request body was not defined")
+var ErrorRequestBodyNotDefined = errors.New("rest: a request body was not defined")
 
-var messageErrResourceSlashesNotAllowed = "resource: slash found on resource name '%s', slashes are not allowed"
-var messageErrParameterNotDefined = "resource: parameter '%s' not defined"
-var messageErrGetURIParamFunctionNotDefined = "resource: no get uri parameter function is defined in context value InputContextKey(\"uriparamfunc\") for '%v' parameter"
-var messageErrFailResponseNotDefined = "resource: resource '%s' failedResponse was not defined, but the operation was expecting one"
+var messageErrResourceSlashesNotAllowed = "rest: slash found on resource name '%s', slashes are not allowed"
+var messageErrParameterNotDefined = "rest: parameter '%s' not defined"
+var messageErrGetURIParamFunctionNotDefined = "rest: no get uri parameter function is defined in context value InputContextKey(\"uriparamfunc\") for '%v' parameter"
+var messageErrFailResponseNotDefined = "rest: resource '%s' failedResponse was not defined, but the operation was expecting one"
 
 // TypeErrorResourceSlashesNotAllowed typed error when a slash character is included in the `name` parameter of a `Resource`.
 type TypeErrorResourceSlashesNotAllowed struct {
