f2dfb701300e3e94cac16b9097a279fec9f74c23
Cristiano's <osheec@gmail.com>
2020-12-14 16:20:49 -0500

    comment correction

--- api.go
+++ api.go
@@ -8,7 +8,7 @@ import (
 )
 
 // API is the root of a REST API abstraction.
-// The two main responsibilities are generate specification generation (GenerateSpec function), and
+// The two main responsibilities are specification generation (GenerateSpec function), and
 // Server handler generation (GenerateServer function).
 type API struct {
 	ID          string
